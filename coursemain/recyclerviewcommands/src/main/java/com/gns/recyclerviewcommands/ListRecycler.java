c800840f0f6c5c77fff1e5348eb59217970b0c40
Muammer Güneş <muammergunes93@gmail.com>
2022-12-17 20:00:17 +0300

    added recyclerview

---
+++ coursemain/recyclerviewcommands/src/main/java/com/gns/recyclerviewcommands/ListRecycler.java
@@ -0,0 +1,81 @@
+package com.gns.recyclerviewcommands;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.gns.recyclerviewcommands.databinding.ActivityMainBinding;
+import com.gns.recyclerviewcommands.databinding.RecyclerviewRowBinding;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class ListRecycler extends RecyclerView.Adapter<ListRecycler.ListHolder> {
+    private static final String TAG = "ListRecycler";
+    private final List<ListModal> list;
+    private final ActivityMainBinding activityMainBinding;
+
+    public ListRecycler(List<ListModal> list, ActivityMainBinding activityMainBinding) {
+        this.list = list;
+        this.activityMainBinding = activityMainBinding;
+    }
+
+    @NonNull
+    @Override
+    public ListHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: viewtype:"+viewType);
+        return new ListHolder(RecyclerviewRowBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: "+list.get(position).getName()+" : "+position);
+        holder.binding.textView.setText(list.get(position).getName());
+        holder.binding.imageView2.setImageResource(android.R.drawable.ic_delete);
+        holder.binding.getRoot().setOnClickListener(view -> {
+            String text = activityMainBinding.editTextTextPersonName.getText().toString();
+            if (text.isEmpty() && position != 0){
+                Collections.swap(list, position, 0);
+                notifyItemMoved(position, 0);
+                this.notifyItemRangeChanged(0,getItemCount());
+                activityMainBinding.recyclerView.scrollToPosition(0);
+            }else {
+                list.get(position).setName(text);
+                this.notifyItemChanged(position);
+                this.notifyItemRangeChanged(position,getItemCount());
+            }
+        });
+        holder.binding.getRoot().setOnLongClickListener(view -> {
+            list.remove(position);
+            this.notifyItemRemoved(position);
+            this.notifyItemRangeChanged(position,getItemCount());
+            return true;
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount: "+list.size());
+        return list.size();
+    }
+
+
+    public static class ListHolder extends RecyclerView.ViewHolder{
+        RecyclerviewRowBinding binding;
+        public ListHolder(@NonNull RecyclerviewRowBinding itemView) {
+            super(itemView.getRoot());
+            Log.d(TAG, "ListHolder: "+itemView.textView.getText().toString());
+            this.binding = itemView;
+        }
+    }
+
+
+}
---
+++ coursemain/recyclerviewcommands/src/main/java/com/gns/recyclerviewcommands/MainActivity.java
@@ -0,0 +1,62 @@
+package com.gns.recyclerviewcommands;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.gns.recyclerviewcommands.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    ListRecycler recyclerAdapter;
+    List<ListModal> list = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //init binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //bir görünüm oluştur
+        View mainView = binding.getRoot();
+        //bu görünümü aktiviteye bağla
+        setContentView(mainView);
+
+        recyclerAdapter = new ListRecycler(list,binding);
+        //binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setAdapter(recyclerAdapter);
+
+        //this will use send from add button for information
+        //Snackbar.make(view,"My Own Action",Snackbar.LENGTH_LONG).setAction("Action",null).show();
+
+        for (int i=0;i<20;i++){
+            list.add(0, new ListModal(getRandomString(12)));
+            recyclerAdapter.notifyItemInserted(0);
+            recyclerAdapter.notifyItemRangeChanged(0,list.size());
+        }
+        binding.recyclerView.scrollToPosition(0);
+        binding.addButton.setOnClickListener(view -> {
+            list.add(0, new ListModal(binding.editTextTextPersonName.getText().toString()));
+            recyclerAdapter.notifyItemInserted(0);
+            recyclerAdapter.notifyItemRangeChanged(0,list.size());
+            binding.recyclerView.scrollToPosition(0);
+        });
+    }
+
+    private static final String ALLOWED_CHARACTERS ="0123456789qwertyuiopasdfghjklzxcvbnm";
+
+    private static String getRandomString(final int sizeOfRandomString)
+    {
+        final Random random=new Random();
+        final StringBuilder sb=new StringBuilder(sizeOfRandomString);
+        for(int i=0;i<sizeOfRandomString;++i)
+            sb.append(ALLOWED_CHARACTERS.charAt(random.nextInt(ALLOWED_CHARACTERS.length())));
+        return sb.toString();
+    }
+}
